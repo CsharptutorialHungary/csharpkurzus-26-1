dc43b91e152d6daf9efff00e447d83260351816b
h373710 <282289493+h373710@users.noreply.github.com>
2026-05-08 16:37:05 +0200

    add Ember class

---
+++ EmberekDB/EmberekDB.Core/Ember.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmberekDB.Core
+{
+    public record class Ember
+    {
+        private readonly string _name;
+        private readonly int _age;
+        private readonly string _gender;
+
+        public Ember(string name, int age, string gender) {
+            _name = name;
+            _age = age;
+            _gender = gender;
+        }
+
+    }
+}
