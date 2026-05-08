025d8b2176d7fc7f08b938d3cb4e12576ea91fc8
h373710 <282289493+h373710@users.noreply.github.com>
2026-05-08 17:21:00 +0200

    add EmberSerializer.cs

---
+++ EmberekDB/EmberekDB.Core/EmberSerializer.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+
+namespace EmberekDB.Core;
+
+internal class EmberSerializer
+{
+    public List<Ember> Load()
+    {
+        if (!File.Exists("emberek.json")) {
+            File.WriteAllText("emberek.json","[]");
+        }
+        string json = File.ReadAllText("emberek.json");
+        return JsonSerializer.Deserialize<List<Ember>>(json) ?? new List<Ember>();
+    }
+    
+
+    public void Save(List<Ember> emberek)
+    {
+        var json = JsonSerializer.Serialize(emberek);
+        File.WriteAllText("emberek.json", json);
+    }
+}
