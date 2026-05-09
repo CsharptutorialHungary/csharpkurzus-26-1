4043cb9c5b8b03243f9895fbcea22b1d66ae99c3
h373710 <282289493+h373710@users.noreply.github.com>
2026-05-09 15:34:22 +0200

    add error handling to EmberSerializer.cs

--- EmberekDB/EmberekDB.Core/EmberSerializer.cs
+++ EmberekDB/EmberekDB.Core/EmberSerializer.cs
@@ -6,17 +6,41 @@ internal class EmberSerializer
 {
     public List<Ember> Load()
     {
+        List<Ember> emberek = new List<Ember>();
         if (!File.Exists("emberek.json")) {
-            File.WriteAllText("emberek.json","[]");
+            try
+            {
+                File.WriteAllText("emberek.json", "[]");
+            }
+            catch (IOException ex) {
+                Console.WriteLine("Hiba történt a file létrehozásakor!");
+                //Console.WriteLine(ex.Message);
+            }
         }
-        string json = File.ReadAllText("emberek.json");
-        return JsonSerializer.Deserialize<List<Ember>>(json) ?? new List<Ember>();
+        try
+        {
+            string json = File.ReadAllText("emberek.json");
+            emberek = JsonSerializer.Deserialize<List<Ember>>(json) ?? [];
+        }
+        catch (IOException ex) {
+            Console.WriteLine("Hiba történt a file beolvasásakor!");
+            //Console.WriteLine(ex.Message);
+        }
+
+        return emberek;
     }
     
 
     public void Save(List<Ember> emberek)
     {
-        var json = JsonSerializer.Serialize(emberek);
-        File.WriteAllText("emberek.json", json);
+        try
+        {
+            var json = JsonSerializer.Serialize(emberek);
+            File.WriteAllText("emberek.json", json);
+        }
+        catch (IOException ex) {
+            Console.WriteLine("A mentés sikertelen!");
+            //Console.WriteLine(ex.Message);
+        }
     }
 }
